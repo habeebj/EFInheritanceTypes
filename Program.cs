5cea57b25849fefe4be2e35947fbd472b979636f
Jimoh Habeeb <habeebjimoh@hotmail.com>
2021-05-14 12:03:02 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFInheritanceTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
